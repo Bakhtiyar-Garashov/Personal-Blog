b0b130c227fbdda9e0b12ac9e12af0a7efdcd82e
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-08-02 02:36:48 +0300

    Ant layout component added

--- client/blog/src/App.js
+++ client/blog/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import 'antd/dist/antd.css'; // And ui css
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap react css
+import CustomLayout from './layout/layout';
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <CustomLayout>
+
+    </CustomLayout>
   );
 }
 
