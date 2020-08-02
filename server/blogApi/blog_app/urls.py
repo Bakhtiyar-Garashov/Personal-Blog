ee6dfc92b8744ade1c509c3d95359c5c57c79eca
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-08-02 18:59:15 +0300

    Id param added to url

--- server/blogApi/blog_app/urls.py
+++ server/blogApi/blog_app/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('v1/', home, name='home'),
     path('v1/posts', get_all_posts, name='posts'),
     path('v1/posts/create', create_post, name='create post'),
-    path('v1/posts/<id>', post_detail, name='post detail'),
+    path('v1/posts/<int:id>', post_detail, name='post detail'),
 ]
