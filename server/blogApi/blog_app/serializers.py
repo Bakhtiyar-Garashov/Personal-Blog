ea5ef8eff6aeb69c57c3799fe33230a164c29212
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-08-04 19:21:24 +0300

    slug field configured as read only

--- server/blogApi/blog_app/serializers.py
+++ server/blogApi/blog_app/serializers.py
@@ -3,6 +3,8 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(read_only=True)
+
     class Meta:
         model = Post
         fields = '__all__'
