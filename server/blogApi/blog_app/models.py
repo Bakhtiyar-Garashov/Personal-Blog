7f3039e5e660240a121a94247ba7de9b899ef8e0
Bakhtiyar-Garashov <qarashov787@gmail.com>
2020-08-04 17:20:22 +0300

    Image field added to model

--- server/blogApi/blog_app/models.py
+++ server/blogApi/blog_app/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
+    image = models.ImageField(upload_to='uploads', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated_at = models.DateTimeField(auto_now=True)
 
