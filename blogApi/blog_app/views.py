72a6cd04ccfcafe283e9c2b74e3dcce1da0993f6
Bakhtiyar-Garashov <bakhtiyar.garashov@ut.ee>
2020-08-13 18:49:57 +0300

    First commit

--- blogApi/blog_app/views.py
+++ blogApi/blog_app/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from rest_framework import status
+from django.db import IntegrityError
 from .models import Post
 from .serializers import PostSerializer
 # Create your views here.
@@ -26,11 +27,16 @@ def get_all_posts(request):
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
 def create_post(request):
-    serialized_post = PostSerializer(data=request.data)
-    if serialized_post.is_valid():
-        serialized_post.save()
-
-    return Response({"success": "New entry added successfully"}, status=status.HTTP_201_CREATED)
+    try:
+        serialized_post = PostSerializer(data=request.data)
+        if serialized_post.is_valid():
+            serialized_post.save()
+            return Response({"success": "New entry added successfully"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"error": "Adding new entry failed"}, status=status.HTTP_400_BAD_REQUEST)
+    except IntegrityError as e:
+        return Response({"failed":str(e)})
+        
 
 
 @api_view(['GET'])
